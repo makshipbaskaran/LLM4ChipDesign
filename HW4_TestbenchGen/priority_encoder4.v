400c33c2851ec2e5f5a500ee4fd1d8aa6ae841a1
makshipbaskaran <mb10197@nyu.edu>
2026-03-12 16:51:12 -0400

    Add files via upload

---
+++ HW4_TestbenchGen/priority_encoder4.v
@@ -0,0 +1,17 @@
+
+module priority_encoder4 (
+    input  wire [3:0] req,
+    output reg  [1:0] code,
+    output reg        valid
+);
+    always @* begin
+        valid = |req;
+        casex (req)
+            4'b1xxx: code = 2'b11;
+            4'b01xx: code = 2'b10;
+            4'b001x: code = 2'b01;
+            4'b0001: code = 2'b00;
+            default: code = 2'b00;
+        endcase
+    end
+endmodule
